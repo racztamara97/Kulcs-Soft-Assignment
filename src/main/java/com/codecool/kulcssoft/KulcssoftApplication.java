4a22438261318e4b871a3f20ed514d0d142787a3
racztamara97 <racztamara97@gmail.com>
2019-02-20 12:19:50 +0100

    Initial commit

---
+++ src/main/java/com/codecool/kulcssoft/KulcssoftApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.kulcssoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KulcssoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KulcssoftApplication.class, args);
+    }
+
+}
