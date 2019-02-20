45942ca41b2954e1c09a61a50301ed86516273f9
racztamara97 <racztamara97@gmail.com>
2019-02-20 12:33:25 +0100

    user's fields (+getters/setters) added

--- src/main/java/com/codecool/kulcssoft/model/User.java
+++ src/main/java/com/codecool/kulcssoft/model/User.java
@@ -1,4 +1,49 @@
 package com.codecool.kulcssoft.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity(name = "Users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer UserId;
+    private String UserName;
+    private String UserEmail;
+
+    public User() {
+    }
+
+    public User(String userName, String userEmail) {
+        UserName = userName;
+        UserEmail = userEmail;
+    }
+
+    public Integer getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(Integer userId) {
+        UserId = userId;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserEmail() {
+        return UserEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        UserEmail = userEmail;
+    }
 }
+
