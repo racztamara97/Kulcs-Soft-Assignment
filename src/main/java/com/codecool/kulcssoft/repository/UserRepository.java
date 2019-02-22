c9e052a824c3ccff3234c8916fae6343691ee138
racztamara97 <racztamara97@gmail.com>
2019-02-22 14:26:34 +0100

    findAll + getUserByUserId

--- src/main/java/com/codecool/kulcssoft/repository/UserRepository.java
+++ src/main/java/com/codecool/kulcssoft/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.codecool.kulcssoft.repository;
 
-public interface UserRepository {
+import com.codecool.kulcssoft.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    List<User> findAll();
+
+    User getUserByUserId(Integer id);
 }
