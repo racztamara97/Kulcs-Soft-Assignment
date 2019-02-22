11faa898bc026764bd8e502b57d2cc24928c5745
racztamara97 <racztamara97@gmail.com>
2019-02-22 14:25:50 +0100

    user delete

--- src/main/java/com/codecool/kulcssoft/service/UserService.java
+++ src/main/java/com/codecool/kulcssoft/service/UserService.java
@@ -1,5 +1,17 @@
 package com.codecool.kulcssoft.service;
 
+import com.codecool.kulcssoft.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void deleteUserById(Integer id){
+        userRepository.delete(userRepository.getUserByUserId(id));
+    }
 
 }
